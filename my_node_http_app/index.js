d650ed10194cb720fb2d34996781b91f61197955
Anita Lipsky <anita@purplebugs.com>
2019-12-03 21:36:44 +0100

    used http npm module to request elasticsearch API, thus packaged all in node module

--- my_node_http_app/index.js
+++ my_node_http_app/index.js
@@ -1,4 +1,5 @@
-var http = require("http");
+var http = require("http"); // Using built-in http node library
+const fetch = require('node-fetch'); // Using module from npm
 
 // To run: node index.js
 console.log('Hello World!');
@@ -8,3 +9,12 @@ console.log('Hello World!');
 http.get("http://localhost:9200", (response) => {
     console.log(response.statusCode);
 });
+
+// Using npm module which is simpler to use
+fetch("http://localhost:9200")
+    .then((res) => {
+       return res.text()
+    })
+    .then((body) => {
+        console.log(body)
+    });
